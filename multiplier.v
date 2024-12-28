7c610305b33041a16d2a9022a49981e1f450022d
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-12-28 22:51:27 +0500

    update: multiplier

--- multiplier.v
+++ multiplier.v
@@ -3,7 +3,7 @@ module Calculator(
     input enable_multiplication,  // Control for enabling multiplication
     input [7:0] A00, A01, A02, A10, A11, A12, A20, A21, A22,
     input [7:0] B00, B01, B02, B10, B11, B12, B20, B21, B22,
-    output reg [15:0] R00, R01, R02, R10, R11, R12, R20, R21, R22 // Changed reg to wire
+    output reg [143:0] result // Single array for result
 );
     
     reg [7:0] A1 [0:2][0:2];
@@ -32,22 +32,15 @@ module Calculator(
             for (i = 0; i < 3; i = i + 1) begin
                 for (j = 0; j < 3; j = j + 1) begin
                     for (k = 0; k < 3; k = k + 1) begin
-                        Res1[i][j] = Res1[i][j] + (A1[i][k] * B1[k][j]);
+                        Res1[i][j] = Res1[i][j] + A1[i][k] * B1[k][j];
                     end
                 end
             end
-            // Assign results to output ports (wires)
-            assign R00 = Res1[0][0];
-            assign R01 = Res1[0][1];
-            assign R02 = Res1[0][2];
-            assign R10 = Res1[1][0];
-            assign R11 = Res1[1][1];
-            assign R12 = Res1[1][2];
-            assign R20 = Res1[2][0];
-            assign R21 = Res1[2][1];
-            assign R22 = Res1[2][2];
+            
+            // Assign result to single array
+            result = {Res1[2][2], Res1[2][1], Res1[2][0],
+                      Res1[1][2], Res1[1][1], Res1[1][0],
+                      Res1[0][2], Res1[0][1], Res1[0][0]};
         end
     end
-
-
 endmodule
