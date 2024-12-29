6429c22222482626809d06624ad3aa68a5cfb76d
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-12-29 12:50:43 +0500

    chore: remove files not in gitignore

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/abdullah/Documents/GitHub/matrix-multiplication-fpga/top_tb_isim_beh.exe" -prj "/home/abdullah/Documents/GitHub/matrix-multiplication-fpga/top_tb_beh.prj" "top_tb" "work.glbl" 
